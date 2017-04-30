60a3f5b1e61553c89cc27001a1875ade9fb95a1c
Benjamin Bougot <bbougot@gmail.com>
2017-04-30 10:36:10 +0200

    Improve trailer performance

--- PopcornApi/Controllers/TrailerController.cs
+++ PopcornApi/Controllers/TrailerController.cs
@@ -4,11 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PopcornApi.Services.Caching;
 using PopcornApi.Services.Logging;
-using YoutubeExtractor;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Cors;
 using PopcornApi.Models.Trailer;
 using System;
+using VideoLibrary;
 
 namespace PopcornApi.Controllers
 {
@@ -36,81 +35,6 @@ namespace PopcornApi.Controllers
             _cachingService = cachingService;
         }
 
-        /// <summary>
-        /// Youtube quality
-        /// </summary>
-        public enum YoutubeStreamingQuality
-        {
-            Low = 0,
-            Medium = 1,
-            High = 2
-        }
-
-         /// <summary>
-        /// Map for defining youtube video quality
-        /// </summary>
-        private static readonly IReadOnlyDictionary<YoutubeStreamingQuality, IEnumerable<int>>
-            StreamingQualityMap =
-                new Dictionary<YoutubeStreamingQuality, IEnumerable<int>>
-                {
-                    {YoutubeStreamingQuality.High, new HashSet<int> {1080, 720}},
-                    {YoutubeStreamingQuality.Medium, new HashSet<int> {480}},
-                    {YoutubeStreamingQuality.Low, new HashSet<int> {360, 240}}
-                };
-
-                        /// <summary>
-        /// Get VideoInfo of a youtube video
-        /// </summary>
-        /// <param name="youtubeLink">The youtube link of a movie</param>
-        /// <param name="qualitySetting">The youtube quality settings</param>
-        /// <returns>The trailer's video info</returns>
-        private async Task<VideoInfo> GetVideoInfoForStreamingAsync(string youtubeLink,
-            YoutubeStreamingQuality qualitySetting)
-        {
-            IEnumerable<VideoInfo> videoInfos = new List<VideoInfo>();
-
-            // Get video infos of the requested video
-            await Task.Run(() => videoInfos = DownloadUrlResolver.GetDownloadUrls(youtubeLink, false));
-
-            // We only want video matching criterias : only mp4 and no adaptive
-            var filtered = videoInfos
-                .Where(info => info.VideoType == VideoType.Mp4 && !info.Is3D && info.AdaptiveType == AdaptiveType.None);
-
-            return GetVideoByStreamingQuality(filtered, qualitySetting);
-        }
-
-        /// <summary>
-        /// Get youtube video depending of choosen quality settings
-        /// </summary>
-        /// <param name="videosToProcess">List of VideoInfo</param>
-        /// <param name="quality">The youtube quality settings</param>
-        /// <returns>The trailer's video info</returns>
-        private VideoInfo GetVideoByStreamingQuality(IEnumerable<VideoInfo> videosToProcess,
-            YoutubeStreamingQuality quality)
-        {
-            while (true)
-            {
-                var videos = videosToProcess.ToList(); // Prevent multiple enumeration
-
-                if (quality == YoutubeStreamingQuality.High)
-                    // Choose high quality Youtube video
-                    return videos.OrderByDescending(x => x.Resolution).FirstOrDefault();
-
-                // Pick the video with the requested quality settings
-                var preferredResolutions = StreamingQualityMap[quality];
-
-                var preferredVideos =
-                    videos.Where(info => preferredResolutions.Contains(info.Resolution))
-                        .OrderByDescending(info => info.Resolution);
-
-                var video = preferredVideos.FirstOrDefault();
-
-                if (video != null) return video;
-                videosToProcess = videos;
-                quality = (YoutubeStreamingQuality) (((int) quality) - 1);
-            }
-        }
-
         // GET api/trailer/q4Zd-LxoK1c
         [HttpGet("{ytTrailerCode}")]
         public async Task<IActionResult> Get(string ytTrailerCode)
@@ -118,23 +42,23 @@ namespace PopcornApi.Controllers
             var cachedTrailer = _cachingService.GetCache(ytTrailerCode);
             if (cachedTrailer == null)
             {
-                var trailer = await GetVideoInfoForStreamingAsync("http://www.youtube.com/watch?v=" + ytTrailerCode, YoutubeStreamingQuality.High);
-                if (trailer != null && trailer.RequiresDecryption)
+                using (var service = Client.For(YouTube.Default))
                 {
-                    await Task.Run(() => DownloadUrlResolver.DecryptDownloadUrl(trailer));
-                    var response = new TrailerResponse {TrailerUrl = trailer.DownloadUrl};
-                    _cachingService.SetCache(ytTrailerCode, JsonConvert.SerializeObject(response), TimeSpan.FromHours(6));
-                    return Json(response);
-                }
+                    var videos = await service.GetAllVideosAsync("https://youtube.com/watch?v=" + ytTrailerCode);
+                    if (videos != null && videos.Any())
+                    {
+                        var trailer = videos.Where(a => a.Format == VideoFormat.Mp4 && !a.Is3D)
+                            .Aggregate((trailer1, trailer2) => trailer1.Resolution > trailer2.Resolution
+                                ? trailer1
+                                : trailer2);
+                        var response = new TrailerResponse {TrailerUrl = await trailer.GetUriAsync()};
+                        _cachingService.SetCache(ytTrailerCode, JsonConvert.SerializeObject(response),
+                            TimeSpan.FromDays(180));
+                        return Json(response);
+                    }
 
-                if (trailer != null && !trailer.RequiresDecryption)
-                {
-                    var response = new TrailerResponse { TrailerUrl = trailer.DownloadUrl };
-                    _cachingService.SetCache(ytTrailerCode, JsonConvert.SerializeObject(response), TimeSpan.FromHours(6));
-                    return Json(response);
+                    return BadRequest();
                 }
-
-                return BadRequest();
             }
 
             return Json(JsonConvert.DeserializeObject<TrailerResponse>(cachedTrailer));
